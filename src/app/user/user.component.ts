90f7f1bb576d48c577972eeeccb8bf8b1e5730a1
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-07-30 12:16:29 +0200

    the objects of the firestore database will show in the console

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -10,6 +10,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
 import { MatCardModule } from '@angular/material/card';
+import { Firestore, collection, doc, collectionData, onSnapshot } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -34,9 +36,24 @@ export class UserComponent {
   positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
   position = new FormControl(this.positionOptions[1]);
   user: User = new User();
+  firestore: Firestore = inject(Firestore);
+  unsub;
+  allUsers = [];
 
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog) {
+    this.unsub = onSnapshot(collection(this.firestore, "users"), (list) => {
+      list.forEach(element => {
+        console.log(element.data());
+        // this.allUsers.push(element);
+      })
+    });
+  }
+
+
+  ngOnDestroy() {
+    this.unsub();
+  }
 
 
   openDialog() {
