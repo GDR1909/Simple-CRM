c91c542ae7e23ccd59a1b84a32df8ca09df53420
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-08-07 19:08:50 +0200

    retrieve user with ID from Firebase database

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, onSnapshot, doc, collection } from '@angular/fire/firestore';
 import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -11,12 +13,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserDetailComponent {
   userId: any = '';
+  user: User = new User();
+  firestore: Firestore = inject(Firestore);
 
 
   constructor(private route: ActivatedRoute) {
     this.route.paramMap.subscribe(paramMap => {
       this.userId = paramMap.get('id');
       console.log('GOT ID:', this.userId);
+      this.getUser();
     });
+  }
+
+
+  getUser() {
+    onSnapshot(doc(collection(this.firestore, 'users'), this.userId), ((user) => {
+      this.user = new User(user.data());
+      console.log('Retrieved user:', this.user);
+    }));
   }
 }
