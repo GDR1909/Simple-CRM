9a5422590ee6fc9ea9f63827aef5e3cf840c8833
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-08-08 19:06:46 +0200

    added a dropdown menu component from angular material in user-detail.component

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -3,13 +3,14 @@ import { Firestore, onSnapshot, doc, collection } from '@angular/fire/firestore'
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
@@ -36,7 +37,12 @@ export class UserDetailComponent {
   }
 
 
-  openAddressDialog() {
+  editUserDetail() {
+    
+  }
+
+
+  editMenu() {
     
   }
 }
